33a75c05abb8a898de38cdf59d48a6427af4b37e
pmin825 <pmin.0825@gmail.com>
2021-04-08 15:57:13 -0700

    create frontend

---
+++ src/components/Form.js
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+import { connect } from "react-redux";
+import { submitQuestion } from "../actions/question_actions";
+
+const Form = ({ submitQuestion }) => {
+    const [name, setName] = useState("");
+    const [text, setText] = useState("");
+    const [repo, setRepo] = useState("");
+    const [live, setLive] = useState("");
+    return (
+        <>
+            <input type="text" placeholder="name" onChange={() => setName()}>
+                {name}
+            </input>
+            <input type="text" placeholder="name" onChange={() => setText()}>
+                {text}
+            </input>
+            <input type="text" placeholder="name" onChange={() => setRepo()}>
+                {repo}
+            </input>
+            <input type="text" placeholder="name" onChange={() => setLive()}>
+                {live}
+            </input>
+        </>
+    );
+};
+
+const mdp = (dispatch) => ({
+    submitQuestion: (question) => dispatch(submitQuestion(question)),
+});
+
+export default connect(null, mdp)(Form);
